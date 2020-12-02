96905b57904b7fec8e47e23cc4eb14b15a7c4365
cbwestaway <charleswestaway@gmail.com>
2020-12-02 14:42:23 -0500

    Minimize Testing
    - Added timing for the two working methods.

--- minimize.py
+++ minimize.py
@@ -3,6 +3,7 @@ SciPy minimize
 """
 from base import *
 from scipy.optimize import minimize
+import timeit
 
 x0 = [0 for i in range(0, N)]
 bounds = [[X_MIN, X_MAX] for i in range(0, N)]
@@ -16,3 +17,24 @@ con7 = {'type': 'ineq', 'fun': g7}
 cons = [con1, con2, con3, con4, con5, con6, con7]
 res = minimize(f, x0, bounds=bounds, constraints=cons)
 print_arr(res.x)
+print(res)
+
+"""
+    Testing Various Minimize Methods
+    * Currently Failing methods: 'Nelder-Mead', 'Powell', 'CG',
+    'BFGS', 'Newton-CG', 'L-BFGS-B', 'TNC', 'COBYLA',
+    'dogleg', 'trust-ncg', 'trust-exact', 'trust-krylov'
+"""
+mtds = ['SLSQP', 'trust-constr']
+for mtd in mtds:
+    print('=============', mtd, '=============')
+    res = minimize(f, x0, bounds=bounds, constraints=cons, method=mtd)
+    print('f(x):', res.fun)
+    print('x:', res.x)
+    print('iterations:', res.nit)
+    # Compute the average time with timeit
+    fun_str = "minimize(f, x0, bounds=bounds, constraints=cons, method=mtd)"
+    # Run many times to reduce the effect of tasks running in parallel
+    # -- this takes a minute
+    elapsed_time = timeit.timeit(fun_str, number=1000, setup='from __main__ import ' + ', '.join(globals()))/1000
+    print('ave. time: (s)', elapsed_time)
